2a7e482d9775aea5ce6dd6f6c7d66a26034c2ad8
Dominik Porada <git@porada.co>
2012-12-09 14:06:30 +0100

    Rewrite demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,33 @@
-window.addEventListener("resizeend", callback, false);
+(function(window, document) {
 
-function callback(event) {
-  var log = document.getElementById(event.type);
+  window.addEventListener('resizeend', logTime, false);
 
-  if ( log && log.nodeType === 1 ) {
-    var entry, date = new Date();
+  /**
+   * Updates the message in the log.
+  */
+  function logTime(event) {
+    var log = document.getElementById(event.type);
 
-    entry  = "<code>" + event.type + "</code> was triggered ";
-    entry += "at&nbsp;<b>" + pad(date.getUTCHours()) + ":";
-    entry += pad(date.getUTCMinutes()) + ":" + pad(date.getUTCSeconds()) + ".";
-    entry += pad(Math.floor(date.getUTCMilliseconds() / 10)) + "</b>";
-
-    log.className = "";
-    log.innerHTML = entry;
+    if ( log && log.nodeType === 1 ) {
+      log.className = '';
+      log.innerHTML = createEntry(event.type, new Date());
+    }
   }
-}
 
-function pad(number) {
-  return ( number < 10 ) ? "0" + number : "" + number;
-}
+  /**
+   * Creates the very string of the log.
+  */
+  function createEntry(trigger, date) {
+    return '<code>' + trigger + '</code> was triggered at <b>' + pad(date.getUTCHours())
+      + ':' + pad(date.getUTCMinutes()) + ':' + pad(date.getUTCSeconds())
+      + '.' + pad(Math.floor(date.getUTCMilliseconds() / 10)) + '</b>';
+  }
+
+  /**
+   * Makes sure that a given number contains a leading zero if necessary.
+  */
+  function pad(number) {
+    return (( number < 10 ) ? '0' : '') + number;
+  }
+
+})(window, document);
